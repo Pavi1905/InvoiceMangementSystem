158446a5edff252d437b47bc66b944aa3325c4ab
pavi1905 <paviaravind1705@gmail.com>
2020-07-31 01:19:24 +0100

    I have finished Validation part of customer export feature

--- InvoiceManagementSystem/src/test/java/stepdefinitions/CustomerExport.java
+++ InvoiceManagementSystem/src/test/java/stepdefinitions/CustomerExport.java
@@ -9,25 +9,43 @@ import org.openqa.selenium.WebElement;
 import pageObjects.ListCustomerPage;
 
 import java.io.File;
+import java.util.List;
 
 public class CustomerExport extends CommonFunctions {
 
     ListCustomerPage listCustomer = new ListCustomerPage(driver);
 
     @When("^I click export option as \"([^\"]*)\"$")
-    public void i_click_export_option_as(String exportExcel)  {
-        for(int i=2;i<4;i++)
-        {
-            WebElement exportMethod = driver.findElement(By.xpath("//*[@id=\"fileData_wrapper\"]/div[1]/div/div/a["+(i+1)+"]/span"));
+    public void i_click_export_option_as(String exportOptions)  {
+        if(exportOptions.equals("Excel")) {
+            WebElement exportMethod = driver.findElement(By.xpath("//*[@id=\"fileData_wrapper\"]/div[1]/div/div/a[3]/span"));
             exportMethod.click();
-            System.out.println(exportExcel);
         }
+        if(exportOptions.equals("csv")) {
+          WebElement  exportMethod = driver.findElement(By.xpath("//*[@id=\"fileData_wrapper\"]/div[1]/div/div/a[2]/span"));
+            exportMethod.click();
+        }
+        if(exportOptions.equals("PDF")) {
 
+          WebElement  exportMethod = driver.findElement(By.xpath("//*[@id=\"fileData_wrapper\"]/div[1]/div/div/a[4]/span"));
+            exportMethod.click();
+            implicitWait(10);
+        }
     }
 
     @Then("^I should be able to download all the customers with filename as \"([^\"]*)\"$")
-    public void i_should_be_able_to_download_all_the_customers_with_filename_as(String expectedFile)
+    public void i_should_be_able_to_download_all_the_customers_with_filename_as(List<String> expectedFile)
     {
-
+        implicitWait(10);
+        File files = new File("C:\\Users\\pavia\\Downloads");
+        File[] totalFiles = files.listFiles();
+        System.out.println(totalFiles.length);
+        for (File file:totalFiles) {
+            if (expectedFile.contains(file.getName())){
+                System.out.println("File is downloaded");
+                break;
+            }
+        }
     }
+
 }
